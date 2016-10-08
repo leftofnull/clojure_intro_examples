c787a930553df18728606ae01e35af89924d4751
Alexander Kahoun <akahoun@demark.com>
2016-10-07 18:43:15 -0700

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clojure-intro-examples "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Desert Code Camp 2016 - Intro to Clojure"
+  :url "https://github.com/leftofnull/clojure_intro_examples"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.391"]]
   :main ^:skip-aot clojure-intro-examples.core
